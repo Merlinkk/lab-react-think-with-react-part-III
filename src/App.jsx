263fbe98bdecabd361f386ac844cfefe4e4a1b38
Merlinkk <anshulkashyap2504@gmail.com>
2024-01-01 14:53:45 +0530

    part III done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+          <img src={'https://kalvium.com/wp-content/uploads/2023/04/Kalvium-Logo-SVG.svg'} className="logo react" alt="React logo" />
+      </div>
+      <h1>Hello Kalvium</h1>
+    </>
+  )
+}
+
+export default App
